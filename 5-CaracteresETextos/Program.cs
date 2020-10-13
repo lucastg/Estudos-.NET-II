313912b0bf8f47ac9424c72623c88d3d535d577b
Lucas Tadeu <30877738+lucastg@users.noreply.github.com>
2020-10-13 11:28:11 -0300

    Add files via upload

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Executando proj 5");
+
+            //charcter
+            char primeiraLetra = ' ';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)61;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos" + 2020;
+            string cursos = 
+@".NET
+- Java
+- JavaScript";
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursos);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
