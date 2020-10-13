a9333585f4c8833f89fe1278ad3689a01f6fd70d
Lucas Tadeu <30877738+lucastg@users.noreply.github.com>
2020-10-13 11:22:20 -0300

    Add files via upload

---
+++ 2-ExercicioLacoDeRepeticaoWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_ExercicioLacoDeRepeticaoWhile
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            for (int linha = 1; linha <= 5; linha++)
+            {
+                for (int coluna = 1; coluna <= 5; coluna++)
+                {
+                    if (coluna > linha)
+                    {
+                        break;
+                    }
+                    Console.Write( coluna );
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
