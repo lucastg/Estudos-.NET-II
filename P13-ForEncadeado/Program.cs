981001e570542753f73343a83ffc28e58b83419f
Lucas Tadeu <30877738+lucastg@users.noreply.github.com>
2020-10-13 11:37:23 -0300

    Add files via upload

---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando proj 13");
+
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //******
+            //*******
+
+            //Forma com o BREAK
+            for (int contadorLinha =0; contadorLinha <= 10;contadorLinha++)
+            {
+                for(int contadorColuna =0; contadorColuna <= 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+
+
+            //forma diferente de fazer
+            for (int contadorLinha = 0; contadorLinha <= 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
