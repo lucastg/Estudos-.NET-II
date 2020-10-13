1f1056656a82bbfae12e4bbaf2549324096bdb63
Lucas Tadeu <30877738+lucastg@users.noreply.github.com>
2020-10-13 11:34:45 -0300

    Add files via upload

---
+++ 10-CalculaPoupanca/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 ");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Apos " + contadorMes + " meses voce tera R$ " + valorInvestido);
+
+                //contadorMes = contadorMes + 1;
+                //contadorMes += 1;
+                contadorMes++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
