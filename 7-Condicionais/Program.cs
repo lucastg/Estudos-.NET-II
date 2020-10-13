32c8bac4524f31c54a07abf361e7855cfe417f13
Lucas Tadeu <30877738+lucastg@users.noreply.github.com>
2020-10-13 11:29:43 -0300

    Add files via upload

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 7");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("Joao possui mais de 18 anos!");
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("Joao nao possui mais de 18 anos, mas esta acompanhado!");
+                }
+                else
+                {
+                    Console.WriteLine("Joao nao possui mais de 18 anos!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
