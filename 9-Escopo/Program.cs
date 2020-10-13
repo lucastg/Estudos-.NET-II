df0e671dca38aad587ecaf8af0bb6c56f7759864
Lucas Tadeu <30877738+lucastg@users.noreply.github.com>
2020-10-13 11:33:01 -0300

    Add files via upload

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 9 ");
+
+            int idadeJoao = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "Joao esta acompanhado!";
+            }
+            else
+            {
+                mensagemAdicional = "Joao NAO esta acompanhado";
+            }
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Nao pode entrar!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
